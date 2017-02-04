faafec06b56c4e70b523265e3a6b29a19198b96e
Tom Lokhorst <tom@lokhorst.eu>
2017-02-04 23:02:13 +0100

    Add makeImagePreview method

--- example/ImagePreviewDemo/CollectionViewController.swift
+++ example/ImagePreviewDemo/CollectionViewController.swift
@@ -25,28 +25,56 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     
     items = [
 
+      // 1
       ImagePreviewCell.ViewModel(
         url: URL(string: "https://raw.githubusercontent.com/Q42/Q42.ImagePreview.swift/master/demo-resources/img1.jpg")!,
         size: CGSize(width: 4157, height: 2811),
         preview: "AQAUAB7/2gAMAwEAAhEDEQA/AILTR7wIu+KOOFgP9YMZ/wAR9K00tRY2/mCWBmRflQA456n7vP0rlrbQbxLEKNdhZmxiJHYgd8cAD8qtzRXNrY7JLgu4TOxlbHX35rGrjK0Ivlj/AF3Kp4enKSuzQuNOmuo2a3bzROdzskeAx9M9cVWPhaUY81VjJGfnIXNSafqKxRFZtUOnsQf3Uasx+p5wKzbjTLyeXdb3Fpf5HMhlcH8dxreGYYlUlJ07ee6/QylhaXtGuf5HNxKPvHJYAck1ZRyVVTyGz19u9FFK7CxFcSuYi7MSQ+0ZOeKYLuaIYVsdaKK6KTZnJI//2Q=="
       ),
-      
+
+      ImagePreviewCell.ViewModel(
+        url: URL(string: "https://raw.githubusercontent.com/Q42/Q42.ImagePreview.swift/master/demo-resources/img1.jpg")!,
+        size: CGSize(width: 4157, height: 2811),
+        preview: "AgAUAB7/2gAMAwEAAhEDEQA/AK1rplzsDPtWNvXn8eK1TClnD5gdSwGMYOAOvp/OuOg0y2EARdQy5x8uMDPpyRV2X9xAEWbeygcHGOvbk1jVxNaMXyxKp0acmrs1ZdLmuU/0fLo53FgMAn9aoTaGIztmdEPozAVJFfWqRNDc3EkTkEFYx8oz+PNZcti0j7rae3kX1kBU/wAzzXTHF4nkTdP9fy2MXQpczXN+h//Q80jc5HA5qeKd2G04x6fSqsfb/PenwdR+P86m7FZCvNIYi2futioJ7yePhW6U5v8AUP8A7wqnddfxreDZDSP/2Q=="
+      ),
+
+      // 2
       ImagePreviewCell.ViewModel(
         url: URL(string: "https://raw.githubusercontent.com/Q42/Q42.ImagePreview.swift/master/demo-resources/img2.jpg")!,
         size: CGSize(width: 8000, height: 3876),
         preview: "AQAOAB7/2gAMAwEAAhEDEQA/AOt1HWtK0u5jtb/UI4pGj3cAttGO5GcZ5xVefSNAubdrmO9t1jU7TIjKVB64Pb8K5y20jTIm3rBJtZccsM9PXFTSaVps67Y4GTrk8Zz29vzFc0c4pxelzollkpLVIsvKlvKILbVLPG7KLvXJI5zx06ViPqkVzJhdWtwBk524B5+lXX8P6f8AL5QmXaMAkrkfjjNQroNmCRI0uTzlAgz/AOO1ss+orW34HP8A2NUf2vxP/9k="
       ),
-      
+
+      ImagePreviewCell.ViewModel(
+        url: URL(string: "https://raw.githubusercontent.com/Q42/Q42.ImagePreview.swift/master/demo-resources/img2.jpg")!,
+        size: CGSize(width: 8000, height: 3876),
+        preview: "AgAOAB7/2gAMAwEAAhEDEQA/AOnuta0W2lFrcXG47ckxguAD2JGetQzW3heSL7StwigHbuBHUdsGueijsI2JEAweMZNWGjsp/wDliFA9Dz19ev61yrNop6JnVLLJNaivd2kZKQ3seEBIA7AdfasGS8tbn/l6CgcjKkf0rWa2sH+ZImUY4wx4x6VRmt9PCjzI2bn+8Rz+BrZZ3D+Uw/seT+0f/9k="
+      ),
+
+      // 3
       ImagePreviewCell.ViewModel(
         url: URL(string: "https://raw.githubusercontent.com/Q42/Q42.ImagePreview.swift/master/demo-resources/img3.jpg")!,
         size: CGSize(width: 4256, height: 2832),
         preview: "AQATAB7/2gAMAwEAAhEDEQA/AG2GhCRQ1vZs4I2iOeYK/T0I/ka2NCuZrVXY6YhsnG1wuGJPPrz0zXN23ifVLu63/Z4EnYL86xBm7Y53D2p2oatrahPsmo2G903hrePD4AHU/iKpVrqzM+VLY7oXOn3NuskE+xEOXSeQKmOehxk9ulZd9f6VLessEts6qoyVuDtz3wDXl+pXOq3LkXlw8jng+Y+eT6DGB07VWVL0Sf6iNvlACluMetXGaTIk01a5NYoBaAjOflPU1dktYXCEpz04JFFFeTV0N2Uru0iEqAK2CxGN56fnWZcFoJ8Ru6gKONxooq6Mm92JH//Z"
       ),
-      
+
+      ImagePreviewCell.ViewModel(
+        url: URL(string: "https://raw.githubusercontent.com/Q42/Q42.ImagePreview.swift/master/demo-resources/img3.jpg")!,
+        size: CGSize(width: 4256, height: 2832),
+        preview: "AgAUAB7/2gAMAwEAAhEDEQA/AI7TSFdN8UG8c/LI4DcexH8jW5pcjQIXmtVMDDtzyOevXpXL2+vanPLuEaJIcfMFBPtzkUt9qOtrgWtxCCV3ZiXBxjHJ5qlVvozO1tjv9+mOizRyBUX7wlbA/DHJ/CsG9udOmnf7M0bKDxmQsPwzivLr2bUp5MXDlmz/ABHPv3HH0FRRi8DnMat7bsYq4zsyG01uf//Q4KGMJCCpPOD1+tWHijYjI7VEn+pX6D+tWW6j6VwT7gzLlhjYgnPOe5qlcFracrGxIwOpz2rRft9TWdqH/HyfoP5VVNtvUSP/2Q=="
+      ),
+
+      // 4
       ImagePreviewCell.ViewModel(
         url: URL(string: "https://raw.githubusercontent.com/Q42/Q42.ImagePreview.swift/master/demo-resources/img4.jpg")!,
         size: CGSize(width: 4288, height: 2848),
         preview: "AQATAB7/2gAMAwEAAhEDEQA/ALI1/wAMOoKa1Z9OhDA9PcVVudb0wofsjCc9iuMVyQ0rw5D0iupD/tT4/kKZIbGEf6JmMjpnn9a9j2mKS6HlqjSv1NybWxuBS3Xrzk//AFqmt9dszkzJJE3psDD8CK5OS6YdJgfbmnQ3MRz5s0hP+zgCppV8W3b8zSdGlb/IrRqHhVmySfeqbMdxGT1oorGTZ0WDJOM0vQ0UUoPUTP/Z"
+      ),
+
+      ImagePreviewCell.ViewModel(
+        url: URL(string: "https://raw.githubusercontent.com/Q42/Q42.ImagePreview.swift/master/demo-resources/img4.jpg")!,
+        size: CGSize(width: 4288, height: 2848),
+        preview: "AgAUAB7/2gAMAwEAAhEDEQA/ALC6v4YYZW+i+nI/mKqXGqadj/RT5vuOlceun+Hoh92Vj7v/AIAVG5s4v+PU7frzXsOriEjzFRp36m9Jqg3ZWMe/NSpqlkVy4ZT6Yz+orkXuGHR/505JoSPmds+2BSpV8T/w5c6VM//Q8o+0OB2P51EZXxnNNPSmnp+Fd0mzCxOGLdaCSOlNT/GlaiDYmf/Z"
       )
       
     ]
--- src/UIImage+ImagePreview.swift
+++ src/UIImage+ImagePreview.swift
@@ -8,13 +8,15 @@
 
 import Foundation
 import UIKit
+import ImageIO
 
 public struct ImagePreviewSettings {
 
   public static var shared: ImagePreviewSettings = ImagePreviewSettings()
 
   public var headers: [UInt8 : Data] = [
-    1 : Data(base64Encoded: "/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAoHBwkHBgoJCAkLCwoMDxkQDw4ODx4WFxIZJCAmJSMgIyIoLTkwKCo2KyIjMkQyNjs9QEBAJjBGS0U+Sjk/QD3/2wBDAQsLCw8NDx0QEB09KSMpPT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT3/wAARCAMBIgACEQEDEQH/xAAfAAABBQEBAQEBAQAAAAAAAAAAAQIDBAUGBwgJCgv/xAC1EAACAQMDAgQDBQUEBAAAAX0BAgMABBEFEiExQQYTUWEHInEUMoGRoQgjQrHBFVLR8CQzYnKCCQoWFxgZGiUmJygpKjQ1Njc4OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4eLj5OXm5+jp6vHy8/T19vf4+fr/xAAfAQADAQEBAQEBAQEBAAAAAAAAAQIDBAUGBwgJCgv/xAC1EQACAQIEBAMEBwUEBAABAncAAQIDEQQFITEGEkFRB2FxEyIygQgUQpGhscEJIzNS8BVictEKFiQ04SXxFxgZGiYnKCkqNTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqCg4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2dri4+Tl5ufo6ery8/T19vf4+fo=", options: [])!
+    1 : Data(base64Encoded: "/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAoHBwkHBgoJCAkLCwoMDxkQDw4ODx4WFxIZJCAmJSMgIyIoLTkwKCo2KyIjMkQyNjs9QEBAJjBGS0U+Sjk/QD3/2wBDAQsLCw8NDx0QEB09KSMpPT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT3/wAARCAMBIgACEQEDEQH/xAAfAAABBQEBAQEBAQAAAAAAAAAAAQIDBAUGBwgJCgv/xAC1EAACAQMDAgQDBQUEBAAAAX0BAgMABBEFEiExQQYTUWEHInEUMoGRoQgjQrHBFVLR8CQzYnKCCQoWFxgZGiUmJygpKjQ1Njc4OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4eLj5OXm5+jp6vHy8/T19vf4+fr/xAAfAQADAQEBAQEBAQEBAAAAAAAAAQIDBAUGBwgJCgv/xAC1EQACAQIEBAMEBwUEBAABAncAAQIDEQQFITEGEkFRB2FxEyIygQgUQpGhscEJIzNS8BVictEKFiQ04SXxFxgZGiYnKCkqNTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqCg4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2dri4+Tl5ufo6ery8/T19vf4+fo=", options: [])!,
+    2 : Data(base64Encoded: "/9j/4AAQSkZJRgABAQAASABIAAD/4QBYRXhpZgAATU0AKgAAAAgAAgESAAMAAAABAAEAAIdpAAQAAAABAAAAJgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAHqADAAQAAAABAAAAFAAAAAD/7QA4UGhvdG9zaG9wIDMuMAA4QklNBAQAAAAAAAA4QklNBCUAAAAAABDUHYzZjwCyBOmACZjs+EJ+/8AAEQgDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9sAQwAJCQkJCQkQCQkQFhAQEBYeFhYWFh4mHh4eHh4mLiYmJiYmJi4uLi4uLi4uNzc3Nzc3QEBAQEBISEhISEhISEhI/9sAQwELDAwSERIfEREfSzMqM0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tL/90ABAAC", options: [])!
   ]
 
   @available(*, unavailable, renamed: "shared")
@@ -34,7 +36,7 @@ extension UIImage {
     let rest = bytes.suffix(from: 5)
 
     guard let header = ImagePreviewSettings.shared.headers[version] else { return nil }
-    guard let (header1, header2) = header.parts else { return nil }
+    guard let (header1, header2) = header.headerParts else { return nil }
 
     var data = Data()
     data.append(header1)
@@ -49,6 +51,25 @@ extension UIImage {
   public convenience init?(body: Data) {
     fatalError()
   }
+
+  public func makeImagePreview() -> (Data, Data)? {
+    guard
+      let sourceData = UIImageJPEGRepresentation(self, 0.9),
+      let imageSource = CGImageSourceCreateWithData(sourceData as CFData, nil)
+    else { return  nil }
+
+    let options: [NSString: Any] = [
+      kCGImageSourceThumbnailMaxPixelSize: CGFloat(30),
+      kCGImageSourceCreateThumbnailFromImageAlways: true
+    ]
+
+    guard
+      let cgImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options as CFDictionary?),
+      let data = UIImageJPEGRepresentation(UIImage(cgImage: cgImage), 0.4)
+    else { return  nil }
+
+    return data.previewParts
+  }
 }
 
 private extension Data {
@@ -60,7 +81,7 @@ private extension Data {
     return bytes
   }
 
-  var parts: (Data, Data)? {
+  var headerParts: (Data, Data)? {
     guard let range = self.range(of: Data(bytes: [0xFF, 0xC0])) else { return nil }
     let sizeStart = range.lowerBound.advanced(by: 5)
 
@@ -68,5 +89,24 @@ private extension Data {
     let header2 = self.subdata(in: sizeStart ..< self.endIndex)
 
     return (header1, header2)
+  }
+
+  var previewParts: (Data, Data)? {
+    guard let rangeC0 = self.range(of: Data(bytes: [0xFF, 0xC0])) else { return nil }
+    guard let rangeDA = self.range(of: Data(bytes: [0xFF, 0xDA])) else { return nil }
+
+    let sizeStart = rangeC0.lowerBound.advanced(by: 5)
+    let sizeEnd = sizeStart.advanced(by: 4)
+
+    let header1 = self.subdata(in: self.startIndex ..< sizeStart)
+    let header2 = self.subdata(in: sizeEnd ..< rangeDA.lowerBound)
+    let header = header1 + header2
+
+
+    let size = self.subdata(in: sizeStart ..< sizeEnd)
+    let body2 = self.subdata(in: rangeDA.lowerBound ..< self.endIndex)
+    let body = Data(bytes: [0x02]) + size + body2
+
+    return (header, body)
   }
 }
